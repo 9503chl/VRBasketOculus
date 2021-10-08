efff5a36636c7ac15c9f024761c1293ba8a77725
9503chl <9503chl@naver.com>
2021-10-08 14:50:27 +0900

    '21.10.08'

---
+++ Assets/Scripts/OnOffHand.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnOffHand : MonoBehaviour
+{
+    [SerializeField] GameObject Onhand;
+    [SerializeField] GameObject Offhand;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.transform.rotation.eulerAngles.x <= 340)
+        {
+            Onhand.SetActive(true);
+            Offhand.SetActive(false);
+        }
+        else
+        {
+            Offhand.SetActive(true); 
+            Onhand.SetActive(false);
+        }
+    }
+}
